e490cd1222a01f491cc25589d413faeade3299cb
JamesRoy <coroneljamesroy@gmail.com>
2025-03-02 18:01:39 +0800

    Your commit message -> changes has made 3/2/2025

--- Project0ne/main.ps1
+++ Project0ne/main.ps1
@@ -1,10 +1,22 @@
 #PSWindowsOperating System Project
 
-Write-Host "Start Now"
+<# Write-Host "Start Now"
 
 for ($i = 0; $i -lt $array.Count; $i++) {
-    <# Action that will repeat until the condition is met #>
+    <# Action that will repeat until the condition is met 
     Write-Host $i
-}
+} 
+#>
 
-$name = Get-childItem -path "c:\users" | select-object name
+$num1 =10
+$num2 =20
+
+$sum = $num1 + $num2
+
+Write-Host "The sum of two numbers is: $sum"
+<# Arrays in powershell#>
+$computer = @("Brand","Tag","ram")
+
+Write-Host $computer[3]
+
+Get-PSDrive | Format-Table root, CurrentLocation
