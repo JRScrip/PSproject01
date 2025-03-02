980b35655238835665f2d9a095d850a2647ba38d
JamesRoy <coroneljamesroy@gmail.com>
2025-03-02 17:06:33 +0800

    Your commit message -> changes has made 3/2/2025

--- Project0ne/main.ps1
+++ Project0ne/main.ps1
@@ -1,3 +1,8 @@
 #PSWindowsOperating System Project
 
 Write-Host "Start Now"
+
+for ($i = 0; $i -lt $array.Count; $i++) {
+    <# Action that will repeat until the condition is met #>
+    Write-Host $i
+}
