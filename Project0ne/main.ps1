b8acd9f9206562b54567b170d69238749de01241
JRScrip <155869096+JRScrip@users.noreply.github.com>
2025-03-02 17:15:37 +0800

    Update main.ps1 3/2/2025
    added a new variable $name /dated 3/2/2025

--- Project0ne/main.ps1
+++ Project0ne/main.ps1
@@ -6,3 +6,5 @@ for ($i = 0; $i -lt $array.Count; $i++) {
     <# Action that will repeat until the condition is met #>
     Write-Host $i
 }
+
+$name = Get-childItem -path "c:\users" | select-object name
